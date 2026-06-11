6e0de6b397fe6dbeb8da56c3834b9fb745bfc181
JoHons22 <josephhons2719@gmail.com>
2026-06-11 15:13:30 -0500

    Update and rename test.py to ServoTest.py

--- ServoTest.py
+++ ServoTest.py
@@ -16,7 +16,7 @@ def set_angle(angle):
     Convert angle (0-180) to duty cycle.
     Typical servo range: 2.5% to 12.5%
     """
-    duty = 2.5 + (angle / 180.0) * 10.0
+    duty = 2 + (angle / 18)
     pwm.ChangeDutyCycle(duty)
     time.sleep(0.5)
     pwm.ChangeDutyCycle(0)  # Reduce servo jitter
@@ -25,12 +25,15 @@ try:
     while True:
         print("0 degrees")
         set_angle(0)
+        time.sleep(1)
 
         print("90 degrees")
         set_angle(90)
+        time.sleep(1)
 
         print("180 degrees")
         set_angle(180)
+        time.sleep(1)
 
         time.sleep(1)
 
