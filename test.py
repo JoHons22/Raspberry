d78f63574d18e357c2cb66502a6fb494e99c41cd
JoHons22 <josephhons2719@gmail.com>
2026-06-11 14:23:09 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,42 @@
+import RPi.GPIO as GPIO
+import time
+
+# GPIO pin connected to servo signal wire
+SERVO_PIN = 18
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(SERVO_PIN, GPIO.OUT)
+
+# Create PWM object at 50 Hz (standard servo frequency)
+pwm = GPIO.PWM(SERVO_PIN, 50)
+pwm.start(0)
+
+def set_angle(angle):
+    """
+    Convert angle (0-180) to duty cycle.
+    Typical servo range: 2.5% to 12.5%
+    """
+    duty = 2.5 + (angle / 180.0) * 10.0
+    pwm.ChangeDutyCycle(duty)
+    time.sleep(0.5)
+    pwm.ChangeDutyCycle(0)  # Reduce servo jitter
+
+try:
+    while True:
+        print("0 degrees")
+        set_angle(0)
+
+        print("90 degrees")
+        set_angle(90)
+
+        print("180 degrees")
+        set_angle(180)
+
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    pass
+
+finally:
+    pwm.stop()
+    GPIO.cleanup()
