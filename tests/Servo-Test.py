6be18e0fab201373bdc8aa1b855ca26f329d902a
JoHons22 <josephhons2719@gmail.com>
2026-07-28 14:48:00 -0500

    Update Servo-Test.py

--- tests/Servo-Test.py
+++ tests/Servo-Test.py
@@ -19,8 +19,12 @@ PWM_TESTS = [
         "gpio": 12,
         "name": "PWM Test 1 - GPIO12",
         "switch_prompt": (
-            "Set the switch bank to route the servo signal line to GPIO12.\n\n"
-            "GPIO12 is BCM GPIO12, physical pin 32."
+            "Set ALL Switches to the DOWN position.\n\n"
+            "Then set ALL W[0-3] Switches to the UP position.\n\n"
+            "These Switches will remain the same for each of the following servo tests.\n\n"
+            "Set the switches for Servo PWM Test 1:\n\n"
+            "Set Switches M1 & M3 to the DOWN position.\n\n"
+            "PWM output being tested: BCM GPIO12\n\n"
         )
     },
     {
@@ -28,8 +32,11 @@ PWM_TESTS = [
         "gpio": 18,
         "name": "PWM Test 2 - GPIO18",
         "switch_prompt": (
-            "Set the switch bank to route the servo signal line to GPIO18.\n\n"
-            "GPIO18 is BCM GPIO18, physical pin 12."
+            "Set the switches for Servo PWM Test 2.\n\n"
+            "Insure ALL W[0-3] Switches are set to the UP position.\n\n"
+            "Set the switches for Servo PWM Test 2:\n\n"
+            "Set Switches M2 to the DOWN position & M3 to the UP position.\n\n"
+            "PWM output being tested: BCM GPIO18\n\n"
         )
     },
     {
@@ -37,8 +44,11 @@ PWM_TESTS = [
         "gpio": 13,
         "name": "PWM Test 3 - GPIO13",
         "switch_prompt": (
-            "Set the switch bank to route the servo signal line to GPIO13.\n\n"
-            "GPIO13 is BCM GPIO13, physical pin 33."
+            "Set the switches for Servo PWM Test 3.\n\n"
+            "Insure ALL W[0-3] Switches are set to the UP position.\n\n"
+            "Set the switches for Servo PWM Test 3:\n\n"
+            "Set Switches M1 to the UP position & M3 to the DOWN position.\n\n"
+            "PWM output being tested: BCM GPIO13\n\n"
         )
     },
     {
@@ -46,8 +56,11 @@ PWM_TESTS = [
         "gpio": 19,
         "name": "PWM Test 4 - GPIO19",
         "switch_prompt": (
-            "Set the switch bank to route the servo signal line to GPIO19.\n\n"
-            "GPIO19 is BCM GPIO19, physical pin 35."
+            "Set the switches for Servo PWM Test 4.\n\n"
+            "Insure ALL W[0-3] Switches are set to the UP position.\n\n"
+            "Set the switches for Servo PWM Test 4:\n\n"
+            "Set Switches M2 & M3 to the UP position.\n\n"
+            "PWM output being tested: BCM GPIO19\n\n"
         )
     }
 ]
